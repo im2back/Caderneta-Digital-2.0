c86dbb70455bde8c79c9f325e14307a73eb8a16d
im2back <jeff.trabalho@outlook.com>
2024-06-26 11:43:45 -0300

    feat: (STOCK-MS) Implement Product Entity and configure H2 database

---
+++ stockms/src/main/java/com/github/im2back/stockms/model/entities/Product.java
@@ -0,0 +1,39 @@
+package com.github.im2back.stockms.model.entities;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Setter
+@Getter
+@Table(name = "tb_product")
+@Entity
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "product_name")
+	private String name;
+	
+	@Column(name = "product_price")
+	private BigDecimal price;
+	
+	@Column(name = "product_code")
+	private Integer code;
+
+}
