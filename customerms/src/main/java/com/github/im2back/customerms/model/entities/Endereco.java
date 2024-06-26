3f26bf1f9502f1dfed3a0c28ea8b903d5d25e310
im2back <jeff.trabalho@outlook.com>
2024-06-25 23:30:20 -0300

    feat: (CUSTOMER-MS) Implement Customer Entity and configure H2 database

---
+++ customerms/src/main/java/com/github/im2back/customerms/model/entities/Endereco.java
@@ -0,0 +1,22 @@
+package com.github.im2back.customerms.model.entities;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Endereco {
+
+	private String streetName;
+
+	private String houseNumber;
+
+	private String complement;
+
+}
