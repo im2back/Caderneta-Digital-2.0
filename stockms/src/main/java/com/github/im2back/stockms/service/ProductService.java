95c3631481e23dd3876b872b18d47dc805c446b5
im2back <jeff.trabalho@outlook.com>
2024-06-26 15:48:54 -0300

    feat: (STOCK-MS) Implement Product Service

--- stockms/src/main/java/com/github/im2back/stockms/service/ProductService.java
+++ stockms/src/main/java/com/github/im2back/stockms/service/ProductService.java
@@ -1,9 +1,27 @@
 package com.github.im2back.stockms.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.github.im2back.stockms.model.entities.Product;
+import com.github.im2back.stockms.repositories.ProductRepository;
 
 @Service
 public class ProductService {
 
+	@Autowired
+	private ProductRepository repository;
 
+	public Product findProductById(Long id) {
+		return repository.findById(id).orElseThrow(() -> new RuntimeException("Not found"));
+	}
+
+	public Product saveNewProduct(Product product) {
+		Product response = repository.save(product);
+		return response;
+	}
+
+	public void deleProductById(Long id) {
+		repository.deleteById(id);
+	}
 }
