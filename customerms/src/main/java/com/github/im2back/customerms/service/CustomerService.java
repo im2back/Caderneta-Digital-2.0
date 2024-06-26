0489d5c01e0c8b4c86d297e0b4ff7b8645e22848
im2back <jeff.trabalho@outlook.com>
2024-06-26 11:05:34 -0300

    feat: (CUSTOMER-MS) Implement Customer Controller layer

--- customerms/src/main/java/com/github/im2back/customerms/service/CustomerService.java
+++ customerms/src/main/java/com/github/im2back/customerms/service/CustomerService.java
@@ -2,6 +2,7 @@ package com.github.im2back.customerms.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.github.im2back.customerms.model.entities.customer.Customer;
 import com.github.im2back.customerms.repositories.CustomerRepository;
@@ -11,17 +12,19 @@ public class CustomerService {
 
 	@Autowired
 	private CustomerRepository repository;
-	
 
+	@Transactional(readOnly = true)
 	public Customer findCustomerById(Long id) {
 		return repository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
 	}
 
+	@Transactional
 	public Customer saveNewCustomer(Customer customerParam) {
 		Customer customerReturn = repository.save(customerParam);
 		return customerReturn;
 	}
-	
+
+	@Transactional
 	public void deleteCustomerById(Long id) {
 		repository.deleteById(id);
 	}
