7dd7c143a2e962f800af9b6af77e4627161e4591
im2back <jefferson.richards2@outlook.com>
2024-12-25 20:54:57 -0300

    feat: (GATEWAY) Configure application.properties and annotate the main class

--- gateway/src/main/java/im2back/com/github/gateway/GatewayApplication.java
+++ gateway/src/main/java/im2back/com/github/gateway/GatewayApplication.java
@@ -2,10 +2,13 @@ package im2back.com.github.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
