c37c5e06df77818e23c9ed7a051b9ab40d2d0e3b
im2back <jefferson.richards2@outlook.com>
2024-12-25 21:16:41 -0300

    Eureka Created

---
+++ eureka/src/test/java/com/github/im2back/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.im2back.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
