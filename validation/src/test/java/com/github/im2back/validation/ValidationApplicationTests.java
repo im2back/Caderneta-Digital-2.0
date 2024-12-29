b56f2949340a838432b655e32bcf9fda8ccd6a4f
im2back <jefferson.richards2@outlook.com>
2024-12-29 01:14:19 -0300

    Validation microservice created

---
+++ validation/src/test/java/com/github/im2back/validation/ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.im2back.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
